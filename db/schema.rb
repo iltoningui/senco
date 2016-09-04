14a5b671279ef6211d6834a3be8997a7dda8553c
Ilton Flora Ingui <iltoningui@outlook.com>
2016-09-04 10:08:08 +0100

    v

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904010736) do
+ActiveRecord::Schema.define(version: 20160904083830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,12 +135,24 @@ ActiveRecord::Schema.define(version: 20160904010736) do
     t.string   "telefone"
     t.integer  "genero"
     t.date     "data_de_nascimento"
-    t.integer  "roles_mask",         default: 12, null: false
+    t.integer  "roles_mask",             default: 12, null: false
     t.string   "nome_da_empresa"
     t.integer  "loja_id"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
     t.index ["loja_id"], name: "index_usuarios_on_loja_id", using: :btree
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
   end
 
 end
